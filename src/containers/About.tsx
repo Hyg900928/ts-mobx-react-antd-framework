e9daf35e2db6f39cf0d6594b3c6fcba0a80a722e
贺运高 <heyungao@shinezone.com>
2019-06-21 16:13:11 +0800

    update

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 class About extends React.Component {
-    render() {
-        return (
-          <div>
-            About
-          </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        About
+      </div>
+    );
+  }
 }
 
-export default About
+export default About;
